f856d30c54b140d195d254b28b393c1d7c3645c1
shiquine <godchildshiquine@gmail.com>
2021-11-09 05:30:18 -0800

    list

---
+++ List.py
@@ -0,0 +1,11 @@
+# list is series of data that will allow you to use it through out your program
+
+friends = ['john', 'Sam', 'Alex']
+# friends = ['john', 6 , 'Alex']
+
+print(friends)
+print(friends[2])  # This is how to select specific elements within
+# The list you would have to access the index value.
+# The index starts with 0 and you are able to go back and forth
+# To get the required index for instance [1] or [-1]
+print(friends[1:])  # This feature allows you to select the index position as well as everything else after that.
